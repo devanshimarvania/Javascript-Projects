ffa66e4f1780e0a99d23dc285fd98fcc23389920
Devanshi Marvaniya <devanshimarvania21@gmail.com>
2025-08-05 14:02:42 +0530

    Add files via upload

---
+++ BasicCalculator.js
@@ -0,0 +1,8 @@
+let num1 = 5;
+let num2 = 8;
+
+console.log(`The sum of ${num1} and ${num2} is : ${num1 + num2}`);
+console.log(`The difference of ${num1} and ${num2} is: ${num1 - num2}`);
+console.log(`The product of ${num1} and ${num2} is   : ${num1 * num2}`);
+console.log(`The division of ${num1} and ${num2} is  : ${num2 !== 0 ? num1 / num2 : "Division by zero not allowed"}`);
+console.log(`The modulus of ${num1} and ${num2} is   : ${num2 !== 0 ? num1 % num2 : "Modulus by zero not allowed"}`);
